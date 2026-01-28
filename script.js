c14046c4116338a3173a710a755a697309244534
kristasmith1118-bit <kristasmith1118@gmail.com>
2026-01-28 13:14:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const question = document.getElementById("question");
+const gif = document.getElementById("main-gif");
+
+// List of messages for the "No" button
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Think again!",
+    "Last chance!",
+    "Ur a HOE!!!",
+    "You will regret thisssss",
+    "You are mean :(",
+    "I won't buy you a cat",
+    "Or a dog",
+    "PLEASEEEE"
+];
+
+let messageIndex = 0;
+
+noBtn.addEventListener("click", () => {
+    // 1. Change the text on the "No" button
+    noBtn.innerHTML = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+
+    // 2. Make the "Yes" button bigger
+    const currentSize = parseFloat(window.getComputedStyle(yesBtn).fontSize);
+    yesBtn.style.fontSize = (currentSize * 1.5) + "px";
+    yesBtn.style.padding = (currentSize * 0.8) + "px " + (currentSize * 1.5) + "px";
+});
+
+yesBtn.addEventListener("click", () => {
+    // Final screen content
+    question.innerHTML = "Yay!!! I love you! ❤️";
+    gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExejNvaWhtOGRkNXJ3OWNmeXdoYjJoejIwZ2tqbWY2YnpwaWpjdjl6ZyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/TjSPQgowhhJdHgvnwA/giphy.gif"; // Success GIF
+    
+    // Hide the buttons
+    noBtn.style.display = "none";
+    yesBtn.style.display = "none";
+});
